971b05355cd9864daccd8945e39a95df235e6990
Stella van der burg <luna4s108@gmail.com>
2020-07-17 11:28:25 +0200

    - Made another attempt at a Custom html helper specifically for displaying customer in view.
    But I am not sure how to pass customer to the html helper as a part of repairOrder in the view and get it all to work.

--- RepairShop/CustomHtmlHelpers/CustomHelper.cs
+++ RepairShop/CustomHtmlHelpers/CustomHelper.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Linq.Expressions;
 using System.Web.Mvc;
 using RepairShop.Models;
+using RepairShop.ViewModels;
 using System.Web.Mvc.Html;
 using System.Text;
 
@@ -71,5 +72,56 @@ namespace RepairShop.CustomHtmlHelpers
 
             return new MvcHtmlString(outerDiv.ToString());
         }*/
+
+        //Attempt 2
+        /*
+        public static IHtmlString DisplayCustomer(this HtmlHelper<VMRepairOrderIndex> htmlHelper, RepairOrder repairOrder)
+        {
+            var outerDiv = new TagBuilder("div");
+            //outerDiv.AddCssClass("pull-left upload-img-wrapper");
+            var label = new TagBuilder("label");
+            //label.AddCssClass("upload-img");
+            //label.MergeAttribute("data-content", model.ButtonText);
+
+            //var image = new TagBuilder("img");
+            //image.AddCssClass("img-responsive");
+            //image.MergeAttribute("src", viewModel.imageSource);
+            //image.MergeAttribute("width", "250");
+            //image.MergeAttribute("height", "250");
+
+            //var textbox = InputExtensions.TextBoxFor(htmlHelper, m => m.ImageName, new { type = "file", style = "display:none" });
+            //var textbox1 = InputExtensions.TextBoxFor(htmlHelper, m => m.Customer.FirstName + " " + m.Customer.LastName);
+            //var textbox2 = InputExtensions.TextBoxFor(htmlHelper, m => m.Customer.Email);
+
+            //var textbox1 = InputExtensions.TextBoxFor(htmlHelper, m => repairOrder.Customer.FirstName + " " + repairOrder.Customer.LastName);
+            //var textbox2 = InputExtensions.TextBoxFor(htmlHelper, m => repairOrder.Customer.Email);
+
+            TagBuilder tb1 = new TagBuilder("div");
+            tb1.Attributes.Add("type", "text");
+            tb1.Attributes.Add("name", "Fullname");
+            tb1.Attributes.Add("value", repairOrder.Customer.FirstName as string + " " + repairOrder.Customer.LastName as string);
+            tb1.Attributes.Add("style", "color:red");
+
+            TagBuilder tb2 = new TagBuilder("div");
+            tb2.Attributes.Add("type", "text");
+            tb2.Attributes.Add("name", "Email");
+            tb2.Attributes.Add("value", repairOrder.Customer.Email as string);
+            tb2.Attributes.Add("style", "color:red");
+
+            StringBuilder htmlBuilder = new StringBuilder();
+            htmlBuilder.Append(label.ToString(TagRenderMode.StartTag));
+            //htmlBuilder.Append(image.ToString(TagRenderMode.Normal));
+            htmlBuilder.Append(label.ToString(TagRenderMode.EndTag));
+            //htmlBuilder.Append(textbox.ToHtmlString());
+            //htmlBuilder.Append(textbox1.ToHtmlString());
+            //htmlBuilder.Append(textbox2.ToHtmlString());
+            htmlBuilder.Append(tb1.ToString());
+            htmlBuilder.Append(tb2.ToString());
+            outerDiv.InnerHtml = htmlBuilder.ToString();
+            var html = outerDiv.ToString(TagRenderMode.Normal);
+
+            return MvcHtmlString.Create(html);
+        }*/
     }
+        
 }
